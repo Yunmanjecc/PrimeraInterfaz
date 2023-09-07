28e7d55b457c480ddc30d44df0a51c13663c4986
Juan Clemente <j.clemente3@alumnos.santotomas.cl>
2023-09-07 16:46:19 -0300

    7 de septiembre

--- app/src/main/java/juan/clemente/primerainterfaz/MainActivity.java
+++ app/src/main/java/juan/clemente/primerainterfaz/MainActivity.java
@@ -3,12 +3,16 @@ package juan.clemente.primerainterfaz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText texto;
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
